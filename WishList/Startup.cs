c598ea1e11ccd43eed201b71a0d9449b1dcde7f6
Andrew Crook <andrewwcrook@gmail.com>
2021-12-14 09:15:54 -0600

    Completed module

--- WishList/Startup.cs
+++ WishList/Startup.cs
@@ -34,7 +34,7 @@ namespace WishList
 
             app.UseRouting();
             app.UseEndpoints(endpoints =>
-                endpoints.MapDefaultControllerRoute());
+                { endpoints.MapDefaultControllerRoute(); });
         }
     }
 }
